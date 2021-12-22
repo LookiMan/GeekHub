024c33a5d9327ab177257fda6b557775938582f7
LookiMan <address_for_work_at_python@ukr.net>
2021-12-22 14:43:25 +0200

    changed task_3.py

--- HT_10/task_3.py
+++ HT_10/task_3.py
@@ -29,6 +29,18 @@ def convert_uah_to_other_currency(
             return amount_to_exchange / float(cours["sale"])
 
 
+def convert_btc_to_usd(amount_to_exchange: int, exchange_courses: str) -> float:
+    for cours in exchange_courses:
+        if cours["ccy"] == "BTC":
+            return amount_to_exchange * float(cours["sale"])
+
+
+def convert_usd_to_btc(amount_to_exchange: int, exchange_courses: str) -> float:
+    for cours in exchange_courses:
+        if cours["ccy"] == "BTC":
+            return amount_to_exchange / float(cours["sale"])
+
+
 def exchanger() -> float:
     available_currencies = (
         "USD",
@@ -61,23 +73,50 @@ def exchanger() -> float:
 
     if exchange_courses:
 
-        if first_currency == "UAH":
-            return convert_uah_to_other_currency(
-                second_currency, amount_to_exchange, exchange_courses
-            )
+        if "BTC" in (first_currency, second_currency):
+            if first_currency == "BTC":
+                usd = convert_btc_to_usd(amount_to_exchange, exchange_courses)
 
-        else:
-            uah = convert_other_currency_to_uah(
-                first_currency, amount_to_exchange, exchange_courses
-            )
+                if second_currency == "USD":
+                    return usd
 
-            if second_currency == "UAH":
-                return uah
-
-            else:
+                uah = convert_other_currency_to_uah("USD", usd, exchange_courses)
+            
                 return convert_uah_to_other_currency(
                     second_currency, uah, exchange_courses
                 )
+
+            else:
+                if first_currency == "UAH":
+                    uah = amount_to_exchange
+                else:
+                    uah = convert_other_currency_to_uah(first_currency, amount_to_exchange, exchange_courses)       
+                
+                usd = convert_uah_to_other_currency("USD", uah, exchange_courses)
+            
+                if second_currency == "USD":
+                    return usd
+
+                return convert_usd_to_btc(usd, exchange_courses)
+
+        else:
+            if first_currency == "UAH":
+                return convert_uah_to_other_currency(
+                    second_currency, amount_to_exchange, exchange_courses
+                )
+
+            else:
+                uah = convert_other_currency_to_uah(
+                    first_currency, amount_to_exchange, exchange_courses
+                )
+
+                if second_currency == "UAH":
+                    return uah
+
+                else:
+                    return convert_uah_to_other_currency(
+                        second_currency, uah, exchange_courses
+                    )
 
     else:
         print(Fore.RED + "[!] Неможливо отримати курси обміну валют")
@@ -87,7 +126,7 @@ def main() -> None:
     result = exchanger()
 
     print(Fore.GREEN + "[>] ==================")
-    print(Fore.GREEN + f"[>] {round(result, 2)}")
+    print(Fore.GREEN + f"[>] {round(result, 3)}")
 
 
 if __name__ == "__main__":
