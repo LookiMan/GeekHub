f375383b04ed5c6e78c08022561d8e367eca0933
LookiMan <address_for_work_at_python@ukr.net>
2022-03-31 03:03:35 +0300

    removed models Message, Chat from chat.models

---
+++ FinalProject/telegram_bot/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+
+from chat.models import Staff
+from telegram_bot.models import User, Chat, Message
+
+
+
+class StaffSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Staff
+        fields = ("id", "username", "first_name", "last_name")
+
+
+class ClientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("uuid", "id", "first_name", "last_name", "username", "image")
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    staff = StaffSerializer()
+
+    created_at_formatted = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = (
+            "umid", "id", "chat", "user", "staff", "reply_to_message", 
+            "text", "photo", "document", "file_name", "caption", 
+            "created_at_formatted"
+        )
+        depth = 2
+
+    def get_created_at_formatted(self, obj):
+        return obj.created_at.strftime("%d-%m-%Y %H:%M:%S")
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True, read_only=True)
+    user = ClientSerializer()
+    staff = StaffSerializer()
+
+    last_message = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Chat
+        fields = (
+            "ucid", "first_name", "last_name", "username",
+            "user", "staff", "messages", "last_message",
+        )
+        depth = 1
+        read_only_fields = ("messages", "last_message")
+
+    def get_last_message(self, obj):
+        return MessageSerializer(obj.messages.order_by('created_at').last()).data
+
+
+class ChatSerializerWithoutMessages(serializers.ModelSerializer):
+    client = ClientSerializer()
+
+    last_message = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Chat
+        fields = ("ucid", "first_name", "client", "last_name",
+                  "username", "last_message")
+        depth = 1
+        read_only_fields = ("last_message")
+
+    def get_last_message(self, obj):
+        return MessageSerializer(obj.messages.order_by('created_at').last()).data
